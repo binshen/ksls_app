bd6266b10b9a5db589479db9cf1a796fedc883f2
bin.shen <bin.shen@openx.com>
2016-05-07 14:33:20 +0800

    debugging

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -8,15 +8,6 @@
 
  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/**
- * 后台画面控制器
- *
- * @package		app
- * @subpackage	core
- * @category	controller
- * @author		yaobin<645894453@qq.com>
- *
- */
 class Index extends MY_Controller {
 
     public function __construct()
@@ -26,5 +17,13 @@ class Index extends MY_Controller {
 
     public function index() {
         $this->load->view('index.html');
+    }
+
+    public function login() {
+
+    }
+
+    public function logout() {
+
     }
 }
--- application/controllers/route.php
+++ application/controllers/route.php
@@ -8,15 +8,6 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/**
- * 后台画面控制器
- *
- * @package		app
- * @subpackage	core
- * @category	controller
- * @author		yaobin<645894453@qq.com>
- *
- */
 class Route extends MY_Controller {
 
     public function __construct()
