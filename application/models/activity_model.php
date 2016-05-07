b815af5c51ce4e57c59c7b2aa70df8190a5d01ee
bin.shen <bin.shen@openx.com>
2016-05-07 15:47:12 +0800

    debugging

--- application/models/activity_model.php
+++ application/models/activity_model.php
@@ -18,16 +18,19 @@ class Activity_model extends MY_Model
         parent::__destruct();
     }
 
-    public function get_company_list() {
-
+    public function get_company_list($id = NULL) {
+        if(empty($id)) {
+            return $this->db->get('company')->result_array();
+        }
+        return $this->db->get_where('company', array('id' => $id))->result_array();
     }
 
     public function get_subsidiary_list($company_id) {
-
+        return $this->db->get_where('subsidiary', array('company_id' => $company_id))->result_array();
     }
 
     public function get_user_list($subsidiary_id) {
-
+        return $this->db->get_where('user', array('subsidiary_id' => $subsidiary_id))->result_array();
     }
 
     public function list_activity() {
