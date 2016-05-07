952132277bbe215ef8a89cfbb007379bb9c90434
bin.shen <bin.shen@openx.com>
2016-05-07 13:55:15 +0800

    debugging

---
+++ application/controllers/route.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bin.shen
+ * Date: 5/7/16
+ * Time: 13:53
+ */
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * 后台画面控制器
+ *
+ * @package		app
+ * @subpackage	core
+ * @category	controller
+ * @author		yaobin<645894453@qq.com>
+ *
+ */
+class Route extends MY_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function review() {
+        $this->load->view('review.html');
+    }
+}
