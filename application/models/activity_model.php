684a2e99f0ab532ef39812c3040ecefd90d33863
bin.shen <bin.shen@openx.com>
2016-05-07 15:04:27 +0800

    debugging

---
+++ application/models/activity_model.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bin.shen
+ * Date: 5/7/16
+ * Time: 15:03
+ */
+
+class Activity_model extends MY_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
+
+}
