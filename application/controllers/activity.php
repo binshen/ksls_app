9a98865012791ab201cb4620adb2d756815d86e3
bin.shen <bin.shen@openx.com>
2016-05-16 14:09:25 +0800

    bug fix

--- application/controllers/activity.php
+++ application/controllers/activity.php
@@ -31,6 +31,8 @@ class Activity extends MY_Controller {
     function _remap($method,$params = array()) {
         if(!$this->session->userdata('login_user_id')) {
             redirect(site_url('/'));
+        } else {
+            return call_user_func_array(array($this, $method), $params);
         }
     }
 
