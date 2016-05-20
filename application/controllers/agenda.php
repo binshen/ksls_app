7184199975a8ea0ffe8c44a949f63b57eede985f
ksyydream <249499109@qq.com>
2016-05-20 16:07:20 +0800

    111

--- application/controllers/agenda.php
+++ application/controllers/agenda.php
@@ -34,18 +34,59 @@ class Agenda extends MY_Controller
     }
 
     function list_agenda_other($page=1){
+        $role_id = $this->session->userdata('login_role_id');
+        $this->assign('role_id', $role_id);
+        if($role_id == 1) {
+            $company_list = $this->agenda_model->get_company_list();
+            $this->assign('company_list', $company_list);
+        }
+
+        if($this->input->POST('company')) {
+            $this->assign('company', $this->input->POST('company'));
+            $subsidiary_list = $this->agenda_model->get_subsidiary_list($this->input->POST('company'), NULL);
+        } else {
+            $company_id = $this->session->userdata('login_company_id');
+            if($role_id < 4) {
+                $subsidiary_list = $this->agenda_model->get_subsidiary_list($company_id, NULL);
+            } else if($role_id == 4) {
+                $subsidiary_id = $this->session->userdata('login_subsidiary_id');
+                $subsidiary_list = $this->agenda_model->get_subsidiary_list($company_id, $subsidiary_id);
+            }
+        }
+        $this->assign('subsidiary_list', $subsidiary_list);
+        if($this->input->POST('subsidiary')) {
+            $this->assign('subsidiary', $this->input->POST('subsidiary'));
+
+            $user_list = $this->agenda_model->get_subsidiary_user_list($this->input->POST('subsidiary'));
+            $this->assign('user_list', $user_list);
+        }
+        if($this->input->POST('user')) {
+            $this->assign('user', $this->input->POST('user'));
+        }
+
+        $company_id = NULL;
+        if($role_id > 1) {
+            $company_id = $this->session->userdata('login_company_id');
+        }
+        $subsidiary_id = NULL;
+        if($role_id >= 4) {
+            $subsidiary_id = $this->session->userdata('login_subsidiary_id');
+        }
+
+        $course_list = $this->agenda_model->get_course();
+        $this->assign('course_list',$course_list);
         if($this->input->POST('status')) {
             $this->assign('status', $this->input->POST('status'));
         }
         if($this->input->POST('course')) {
             $this->assign('course', $this->input->POST('course'));
         }
-        $data = $this->agenda_model->list_agenda($page, $this->session->userdata('login_user_id'));
+        $data = $this->agenda_model->list_agenda($page, $this->session->userdata('login_user_id'),$subsidiary_id,$company_id);
         $this->assign('agenda_list', $data);
 
-        $pager = $this->pagination->getPageLink('/agenda/list_agenda', $data['countPage'], $data['numPerPage']);
+        $pager = $this->pagination->getPageLink('/agenda/list_agenda_other', $data['countPage'], $data['numPerPage']);
         $this->assign('pager', $pager);
-        $this->display('list_agenda.html');
+        $this->display('list_agenda_other.html');
     }
 
     public function add_agenda() {
--- application/models/agenda_model.php
+++ application/models/agenda_model.php
@@ -17,21 +17,56 @@ class Agenda_model extends MY_Model
         parent::__destruct();
     }
 
-    function list_agenda($page,$user_id = null){
+    public function get_company_list($id = NULL) {
+        if(empty($id)) {
+            return $this->db->get('company')->result_array();
+        }
+        return $this->db->get_where('company', array('id' => $id))->result_array();
+    }
+
+    public function get_subsidiary_list($company_id, $subsidiary_id=NULL) {
+        if(empty($subsidiary_id)) {
+            return $this->db->get_where('subsidiary', array('company_id' => $company_id))->result_array();
+        } else {
+            return $this->db->get_where('subsidiary', array('id' => $subsidiary_id))->result_array();
+        }
+    }
+
+    public function get_subsidiary_user_list($subsidiary_id) {
+        return $this->db->get_where('user', array('subsidiary_id' => $subsidiary_id))->result_array();
+    }
+
+    function list_agenda($page,$user_id = null,$subsidiary_id=null,$company_id=null){
         // 每页显示的记录条数，默认20条
         $numPerPage = $this->input->post('numPerPage') ? $this->input->post('numPerPage') : 1;
         $pageNum = $this->input->post('pageNum') ? $this->input->post('pageNum') : $page;
 
         $this->db->select('count(1) as num');
-        $this->db->from('agenda');
+        $this->db->from('agenda a');
+        $this->db->join('user b','a.user_id = b.id','inner');
         if(!empty($user_id)){
-            $this->db->where('user_id',$user_id);
+            $this->db->where('a.user_id',$user_id);
         }
         if($this->input->post('status')){
-            $this->db->where('status',$this->input->post('status'));
+            $this->db->where('a.status',$this->input->post('status'));
         }
         if($this->input->post('course')){
-            $this->db->where('course',$this->input->post('course'));
+            $this->db->where('a.course',$this->input->post('course'));
+        }
+        if($this->input->POST('company')) {
+            $this->db->where('b.company_id', $this->input->POST('company'));
+        }
+        if($this->input->POST('subsidiary')) {
+            $this->db->where('b.subsidiary_id', $this->input->POST('subsidiary'));
+        }
+        if($this->input->POST('user')) {
+            $this->db->where('b.id', $this->input->POST('user'));
+        }
+        if(!empty($subsidiary_id)) {
+            $this->db->where('b.subsidiary_id', $subsidiary_id);
+        }
+        if(!empty($company_id)) {
+            $this->db->where('b.company_id', $company_id);
         }
         $row = $this->db->get()->row_array();
         //总记录数
@@ -50,6 +85,22 @@ class Agenda_model extends MY_Model
         if($this->input->post('course')){
             $this->db->where('a.course',$this->input->post('course'));
         }
+        if($this->input->POST('company')) {
+            $this->db->where('b.company_id', $this->input->POST('company'));
+        }
+        if($this->input->POST('subsidiary')) {
+            $this->db->where('b.subsidiary_id', $this->input->POST('subsidiary'));
+        }
+        if($this->input->POST('user')) {
+            $this->db->where('b.id', $this->input->POST('user'));
+        }
+        if(!empty($subsidiary_id)) {
+            $this->db->where('b.subsidiary_id', $subsidiary_id);
+        }
+        if(!empty($company_id)) {
+            $this->db->where('b.company_id', $company_id);
+        }
+
         $this->db->limit($numPerPage, ($pageNum - 1) * $numPerPage );
         $this->db->order_by('a.cdate', 'desc');
         $this->db->order_by('a.user_id', 'desc');
@@ -58,5 +109,14 @@ class Agenda_model extends MY_Model
         $data['numPerPage'] = $numPerPage;
         return $data;
 
+    }
+
+    function get_course(){
+        $res = $this->db->select()->from('course')->get()->result_array();
+        if(!$res){
+            return 1;
+        }else{
+            return $res;
+        }
     }
 }
