2fb420f6cb386c90f552e642d974a8cef1dbae3a
zhouxiaohui123 <zxh1989117>
2016-05-26 14:48:25 +0800

    123123

--- application/views/manage/add_activity_type.php
+++ application/views/manage/add_activity_type.php
@@ -1,7 +1,7 @@
 <style type="text/css">
-    .file-box{ position:relative;width:340px}
-    .btn{ background-color:#FFF; border:1px solid #CDCDCD;height:21px; width:70px;}
-    .file{ position:absolute; top:0; right:80px; height:24px; filter:alpha(opacity:0);opacity: 0;width:300px }
+.file-box{ position:relative;width:340px}
+.btn{ background-color:#FFF; border:1px solid #CDCDCD;height:21px; width:70px;}
+.file{ position:absolute; top:0; right:80px; height:24px; filter:alpha(opacity:0);opacity: 0;width:300px }
 </style>
 <div class="pageContent">
     <form method="post" enctype="multipart/form-data" action="<?php echo site_url('manage/save_activity_type');?>" class="pageForm required-validate" onsubmit="return validateCallback(this, dialogAjaxDone);">
