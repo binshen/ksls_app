6867a9e1a62ebd9623650272298dc00a4f274002
ksyydream <249499109@qq.com>
2016-06-12 13:49:36 +0800

    20160608

--- application/controllers/examination.php
+++ application/controllers/examination.php
@@ -10,27 +10,102 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Examination extends MY_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('examination_model');
+    }
+
+    function _remap($method, $params = array())
+    {
+        if(!$this->session->userdata('login_user_id')) {
+            redirect(site_url('/'));
+        } else {
+            return call_user_func_array(array($this, $method), $params);
+        }
+    }
+
     public function self_examination()
     {
 
+        $question_type_list = $this->examination_model->get_question_type_list();
+        $this->assign('question_type_list', $question_type_list);
+        
         $this->display('self_examination.html');
     }
 
-    public function do_examination()
+    public function do_examination($type_id=1, $exam_id=NULL, $num=1)
     {
+        $user_id = $this->session->userdata('login_user_id');
+        if(empty($exam_id)) {
+            $exam = $this->examination_model->get_user_exam($user_id, $type_id);
+            if(!empty($exam)) {
+                $exam_id = $exam['id'];
+            } else {
+                $exam_id = $this->examination_model->gen_exam_data($user_id, $type_id);
+            }
+        }
+        $this->assign('exam_id', $exam_id);
+
+        if(!empty($_POST['eq_id']) && !empty($_POST['option'])) {
+            $op = $_POST['option'];
+            $data = array('as1' => 0, 'as2' => 0, 'as3' => 0, 'as4' => 0, 'complete' => 0);
+            if($op == 'A') {
+                $data['as1'] = 1;
+                $data['complete'] = 1;
+            } else if($op == 'B') {
+                $data['as2'] = 1;
+                $data['complete'] = 1;
+            } else if($op == 'C') {
+                $data['as3'] = 1;
+                $data['complete'] = 1;
+            } else if($op == 'D') {
+                $data['as4'] = 1;
+                $data['complete'] = 1;
+            }
+            $this->examination_model->take_exam($_POST['eq_id'], $data);
+        }
+
+        $exam_data = $this->examination_model->get_exam_by_num($exam_id, $num);
+        $this->assign('exam_data', $exam_data);
+
+        $question_data = $this->examination_model->get_exam_question($exam_id);
+        $this->assign('question_data', $question_data);
+
+        $this->assign('num', $num);
 
         $this->display('do_examination.html');
     }
 
-    public function submit_examination()
-    {
+    public function complete_examination($exam_id) {
 
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $this->examination_model->complete_examination($exam_id);
+            redirect(site_url('/examination/submit_examination/' . $exam_id));
+        } else {
+            redirect(site_url('/examination/self_examination'));
+        }
+    }
+
+    public function submit_examination($exam_id,$question_id=null)
+    {
+        $exam_data = $this->examination_model->get_sub_exam_list($exam_id, $question_id);
+        $this->assign('exam_data', $exam_data);
+        $question_true = $this->examination_model->get_true_exam_question($exam_id);
+        $this->assign('question_true', $question_true);
+        $this->assign('exam_id', $exam_id);
         $this->display('submit_examination.html');
     }
 
-     public function unit_examination()
-     {
+    public function unit_examination()
+    {
 
-         $this->display('unit_examination.html');
-     }
+        $this->display('unit_examination.html');
+    }
+
+    public function chenge_option($eq_id,$val,$as){
+        $this->examination_model->chenge_option($eq_id,$val,$as);
+    }
 }
---
+++ application/models/examination_model.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bin.shen
+ * Date: 6/8/16
+ * Time: 17:07
+ */
+
+class Examination_model extends MY_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
+
+    public function get_question_type_list()
+    {
+        return $this->db->get('question_type')->result_array();
+    }
+
+    public function get_user_exam($user_id, $type_id)
+    {
+        return $this->db->get_where('self_exam', array(
+            'user_id' => $user_id,
+            'type_id' => $type_id,
+            'complete' => 0))->row_array();
+    }
+
+    public function gen_exam_data($user_id, $type_id, $limit=20) {
+        $this->db->trans_start();//--------开始事务
+
+        $this->db->from('question');
+        $this->db->where('type_id', $type_id);
+        $this->db->order_by('RAND()');
+        $this->db->limit($limit);
+        $questions = $this->db->get()->result_array();
+        $exam_data = array();
+        if(!empty($questions)) {
+            $data = array(
+                'user_id' => $user_id,
+                'type_id' => $type_id,
+                'title' => $user_id . '-自助考试-' . date('YmdHis'),
+                'complete' => 0,
+                'created' => date("Y-m-d H:i:s")
+            );
+            $this->db->insert('self_exam', $data);
+            $exam_id = $this->db->insert_id();
+
+            foreach ($questions as $q) {
+                $exam_data[] = array(
+                    'exam_id' => $exam_id,
+                    'question_id' => $q['id'],
+                    'complete' => 0
+                );
+            }
+            $this->db->insert_batch('self_exam_question', $exam_data);
+        }
+
+        $this->db->trans_complete();//------结束事务
+        if ($this->db->trans_status() === FALSE) {
+            return -1;
+        } else {
+            return $exam_id;
+        }
+    }
+
+    public function get_exam_by_num($exam_id, $num) {
+        $this->db->select('c.title, c.op1, c.op2, c.op3, c.op4, c.type_id, d.name AS question_type, b.as1, b.as2, b.as3, b.as4, b.id AS eq_id');
+        $this->db->from('self_exam a');
+        $this->db->join('self_exam_question b', 'a.id = b.exam_id', 'inner');
+        $this->db->join('question c', 'b.question_id = c.id', 'inner');
+        $this->db->join('question_type d', 'c.type_id = d.id', 'inner');
+        $this->db->where('a.id', $exam_id);
+        $this->db->order_by('b.id ASC');
+        $this->db->limit(1);
+        $this->db->offset($num-1);
+        return $this->db->get()->row_array();
+    }
+
+    public function get_exam_question($exam_id) {
+        return $this->db->get_where('self_exam_question', array('exam_id' => $exam_id))->result_array();
+    }
+
+    public function take_exam($eq_id, $data) {
+        $this->db->trans_start();//--------开始事务
+
+        $this->db->where('id', $eq_id);
+        $this->db->update('self_exam_question', $data);
+
+        $this->db->trans_complete();//------结束事务
+        if ($this->db->trans_status() === FALSE) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    public function complete_examination($exam_id) {
+        $this->db->trans_start();//--------开始事务
+
+        $this->db->where('id', $exam_id);
+        $this->db->update('self_exam', array('complete' => 1));
+
+        $this->db->trans_complete();//------结束事务
+        if ($this->db->trans_status() === FALSE) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    public function get_true_exam_question($exam_id) {
+        $this->db->select('a.question_id,c.as1,c.as2,c.as3,c.as4,
+        a.as1 self_as1,a.as2 self_as2,a.as3 self_as3,a.as4 self_as4');
+        $this->db->from('self_exam_question a');
+        $this->db->join('question c', 'a.question_id = c.id', 'inner');
+        $this->db->where('a.exam_id',$exam_id);
+        return $this->db->get()->result_array();
+    }
+
+    public function get_sub_exam_list($exam_id, $question_id = NULL) {
+        $this->db->select('a.complete,b.id self_id,c.title, c.op1, c.op2, c.op3, c.op4,
+        c.as1,c.as2,c.as3,c.as4,
+        b.as1 self_as1,b.as2 self_as2,b.as3 self_as3,b.as4 self_as4,
+        d.name AS question_type');
+        $this->db->from('self_exam a');
+        $this->db->join('self_exam_question b', 'a.id = b.exam_id', 'inner');
+        $this->db->join('question c', 'b.question_id = c.id', 'inner');
+        $this->db->join('question_type d', 'c.type_id = d.id', 'inner');
+        $this->db->where('a.id', $exam_id);
+        if(!empty($question_id)) {
+            $this->db->where('c.id', $question_id);
+        }
+        $this->db->order_by('b.id ASC');
+        $this->db->limit(1);
+        $data['question_detail'] = $this->db->get()->row_array();
+       // echo $this->db->last_query();
+       //die(var_dump($data['question_detail']));
+        $this->db->select('count(1) as num');
+        $this->db->from('self_exam_question a');
+        $this->db->where('a.exam_id',$exam_id);
+        $this->db->where('a.id <=', $data['question_detail']['self_id']);
+        $row = $this->db->get()->row_array();
+        $this->db->select('count(1) as num');
+        $this->db->from('self_exam_question a');
+        $this->db->where('a.exam_id',$exam_id);
+        $row_count = $this->db->get()->row_array();
+        $data['No_question'] = $row['num'];
+        $data['count'] = $row_count['num'];
+        return $data;
+    }
+
+    public function chenge_option($eq_id,$val,$as){
+        $data = array('as1' => 0, 'as2' => 0, 'as3' => 0, 'as4' => 0, 'complete' => 0);
+        if($val == 'A') {
+            $data['as1'] = $as;
+            $data['complete'] = 1;
+        } else if($val == 'B') {
+            $data['as2'] = $as;
+            $data['complete'] = 1;
+        } else if($val == 'C') {
+            $data['as3'] = $as;
+            $data['complete'] = 1;
+        } else if($val == 'D') {
+            $data['as4'] = $as;
+            $data['complete'] = 1;
+        }
+        $this->db->where('id', $eq_id);
+        $this->db->update('self_exam_question', $data);
+    }
+}
