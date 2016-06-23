0316cce2f7902a38f163e62187118134cc7fed7d
ksyydream <249499109@qq.com>
2016-06-23 15:04:12 +0800

    20160621

--- application/models/examination_model.php
+++ application/models/examination_model.php
@@ -536,6 +536,7 @@ class Examination_model extends MY_Model
             'permission_id' =>$this->session->userdata('login_permission_id'),
             'title' => $this->input->post('title'),
             'p_num' => $this->input->post('p_num'),
+            'style' => $this->input->post('style'),
             'start_time' => $this->input->post('start_date'),
             'end_time' => $this->input->post('end_date'),
             'p_score' => $this->input->post('p_score')
