b2062815921e38109e2acbe6013d8a323417d0c7
ksyydream <249499109@qq.com>
2016-08-11 14:35:26 +0800

    20160722

--- application/controllers/alipay.php
+++ application/controllers/alipay.php
@@ -89,7 +89,7 @@ class Alipay extends CI_Controller {
         require_once(APPPATH.'libraries/alipay/alipay_notify.class.php');
         $alipayNotify = new AlipayNotify($this->alipay_config);
         $verify_result = $alipayNotify->verifyReturn();
-        var_dump($_POST);
+        var_dump($_GET);
         var_dump($verify_result);
         if($verify_result) {//验证成功
             /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
