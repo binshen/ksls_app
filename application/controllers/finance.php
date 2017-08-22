25518496d22356a636b57d47f95ed12f15811db0
ksyydream <249499109@qq.com>
2017-08-22 21:38:55 +0800

    20170822

---
+++ application/controllers/finance.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bin.shen
+ * Date: 5/31/16
+ * Time: 16:23
+ */
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Finance extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('finance_model');
+    }
+
+    function _remap($method,$params = array()) {
+        if(!$this->session->userdata('login_user_id') || in_array(1,$this->session->userdata('login_position_id_array'))) {
+            redirect(site_url('/'));
+        } else {
+            return call_user_func_array(array($this, $method), $params);
+        }
+    }
+
+    public function finance_list($page=1){
+        $data = $this->finance_model->finance_list($page);
+        $this->assign('finance_list', $data);
+        $pager = $this->pagination->getPageLink('/finance/finance_list', $data['countPage'], $data['numPerPage']);
+        $this->assign('pager', $pager);
+        $this->display('finance_list.html');
+    }
+
+
+    public function finance_detail($id){
+        $data = $this->finance_model->get_detail($id);
+        $this->assign('data', $data);
+        $this->display('tax_calculate.html');
+    }
+
+    public function add_finance($id = ""){
+        if($id != ""){
+            $data = $this->finance_model->get_detail($id);
+            if($data)
+                $this->assign('data', $data);
+        }
+        $this->display('finance.html');
+    }
+
+    public function save_finance_1(){
+        $res = $this->finance_model->save_finance_1();
+        if($res > 0){
+            redirect(site_url('/finance/edit_finance_2')."/".$res);
+        }else{
+            redirect(site_url('/finance/add_finance'));
+        }
+    }
+
+    public function save_finance_2(){
+        $id = $this->input->post("id");
+        if(!$id)
+            redirect(site_url('/finance/add_finance'));
+        $res = $this->finance_model->save_finance_2();
+        if($res > 0){
+            redirect(site_url('/finance/edit_finance_3')."/".$res);
+        }else{
+            redirect(site_url('/finance/add_finance'));
+        }
+    }
+
+    public function edit_finance_2($id){
+        if(!$id)
+            redirect(site_url('/finance/add_finance'));
+        $data = $this->finance_model->get_detail($id);
+        $this->assign('data', $data);
+        $this->display('finance - step2.html');
+    }
+
+}
---
+++ application/models/finance_model.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bin.shen
+ * Date: 5/31/16
+ * Time: 23:00
+ */
+
+class Finance_model extends MY_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
+
+    public function Finance_list($page){
+        // 每页显示的记录条数，默认20条
+        $numPerPage = $this->input->post('numPerPage') ? $this->input->post('numPerPage') : 10;
+        $pageNum = $this->input->post('pageNum') ? $this->input->post('pageNum') : $page;
+
+        $this->db->select('count(1) as num');
+        $this->db->from('fj_xiaoqu a');
+        $this->db->join('fj_xiaoqu_detail b','a.id = b.xiaoqu_id','inner');
+        $this->db->join('fj_xiaoqu_type c','b.type_id = c.id','inner');
+        if ($this->input->post('xiaoqu')){
+            $this->db->like('a.xiaoqu',trim($this->input->post('xiaoqu')));
+        }
+        $this->db->where(array(
+            'a.flag'=>1,
+            'b.pgj >'=>0
+        ));
+        $row = $this->db->get()->row_array();
+        //总记录数
+        $data['countPage'] = $row['num'];
+        $data['xiaoqu'] = $this->input->post('xiaoqu') ? trim($this->input->post('xiaoqu')) : "";
+        //list
+        $this->db->select('a.*,b.*,c.type_name,b.id detail_id');
+        $this->db->from('fj_xiaoqu a');
+        $this->db->join('fj_xiaoqu_detail b','a.id = b.xiaoqu_id','inner');
+        $this->db->join('fj_xiaoqu_type c','b.type_id = c.id','inner');
+        if ($this->input->post('xiaoqu')){
+            $this->db->like('a.xiaoqu',trim($this->input->post('xiaoqu')));
+        }
+        $this->db->where(array(
+            'a.flag'=>1,
+            'b.pgj >'=>0
+        ));
+        $this->db->limit($numPerPage, ($pageNum - 1) * $numPerPage );
+        $this->db->order_by('a.id', 'desc');
+        $data['res_list'] = $this->db->get()->result_array();
+        $data['pageNum'] = $pageNum;
+        $data['numPerPage'] = $numPerPage;
+        return $data;
+    }
+
+
+    public function get_detail($id){
+        $this->db->select('*');
+        $this->db->from('finance a');
+        //$this->db->join('fj_xiaoqu_detail b','a.id = b.xiaoqu_id','inner');
+        //$this->db->join('fj_xiaoqu_type c','b.type_id = c.id','inner');
+        $this->db->where(array(
+            'a.id'=>$id
+        ));
+        $row= $this->db->get()->row_array();
+        return $row;
+    }
+
+    public function save_finance_1(){
+        $id = $this->input->post("id");
+        $data = array(
+            "borrower_name" => trim($this->input->post("borrower_name")),
+            "borrower_age" => $this->input->post("borrower_age"),
+            "borrower_sex" => $this->input->post("borrower_sex"),
+            "borrower_native" => trim($this->input->post("borrower_native")),
+            "borrower_qualifications" => $this->input->post("borrower_qualifications"),
+            "borrower_marriage" => $this->input->post("borrower_marriage"),
+            "borrower_workADD" => trim($this->input->post("borrower_workADD")),
+            "borrower_position" => trim($this->input->post("borrower_position")),
+            "borrower_income" => trim($this->input->post("borrower_income")),
+            "borrower_SSY" => trim($this->input->post("borrower_SSY")),
+            "borrower_code" => trim($this->input->post("borrower_code")),
+            "borrower_phone" => trim($this->input->post("borrower_phone")),
+            //下面是配偶信息
+            "spouse_name" => trim($this->input->post("spouse_name")),
+            "spouse_sex" => $this->input->post("spouse_sex"),
+            "spouse_native" => $this->input->post("spouse_native"),
+            "spouse_age" => $this->input->post("spouse_age"),
+            "spouse_qualifications" => trim($this->input->post("spouse_qualifications")),
+            "spouse_workADD" => trim($this->input->post("spouse_workADD")),
+            "spouse_position" => trim($this->input->post("spouse_position")),
+            "spouse_income" => $this->input->post("spouse_income"),
+            "spouse_SSY" => trim($this->input->post("spouse_SSY")),
+            "spouse_code" => trim($this->input->post("spouse_code")),
+            "spouse_phone" => trim($this->input->post("spouse_phone")),
+            //亲属
+            "relatives1name" => trim($this->input->post("relatives1name")),
+            "relatives1tie" => trim($this->input->post("relatives1tie")),
+            "relatives1age" => $this->input->post("relatives1age"),
+            "relatives1position" => trim($this->input->post("relatives1position")),
+            "relatives1native" => trim($this->input->post("relatives1native")),
+            "relatives2name" => trim($this->input->post("relatives2name")),
+            "relatives2tie" => trim($this->input->post("relatives2tie")),
+            "relatives2age" => $this->input->post("relatives2age"),
+            "relatives2position" => trim($this->input->post("relatives2position")),
+            "relatives2native" => trim($this->input->post("relatives2native")),
+            "relatives3name" => trim($this->input->post("relatives3name")),
+            "relatives3tie" => trim($this->input->post("relatives3tie")),
+            "relatives3age" => $this->input->post("relatives3age"),
+            "relatives3position" => trim($this->input->post("relatives3position")),
+            "relatives3native" => trim($this->input->post("relatives3native")),
+            "relatives4name" => trim($this->input->post("relatives4name")),
+            "relatives4tie" => trim($this->input->post("relatives4tie")),
+            "relatives4age" => $this->input->post("relatives4age"),
+            "relatives4position" => trim($this->input->post("relatives4position")),
+            "relatives4native" => trim($this->input->post("relatives4native")),
+
+
+        );
+        $this->db->trans_start();
+        if(!$id){
+            $data["user_id"] = $this->session->userdata('login_user_id');
+            $data["create_date"] = date('Y-m-d H:i:s');
+            $data["status"] = 1;
+            $this->db->insert("finance",$data);
+            $id = $this->db->insert_id();
+
+        }else{
+            $this->db->where('id',$id)->update("finance",$data);
+        }
+
+        $this->db->trans_complete();//------结束事务
+        if ($this->db->trans_status() === FALSE) {
+            return -1;
+        } else {
+            return $id;
+        }
+
+    }
+
+    public function save_finance_2(){
+
+    }
+
+    public function check_power($id){
+
+    }
+
+}
