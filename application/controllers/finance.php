86da9a2f2f8ba6fa5df6496be202e225f1270185
ksyydream <249499109@qq.com>
2017-09-04 10:34:07 +0800

    20170823

--- application/controllers/finance.php
+++ application/controllers/finance.php
@@ -374,30 +374,14 @@ class Finance extends MY_Controller
     public function show_img($id){
         //$code = $this->set_base_code($id);
         require_once (APPPATH . 'libraries/phpqrcode.php');
-        $value = $id;//二维码内容
+        $value = 'http://www.cnblogs.com/txw1958/'; //二维码内容
         $errorCorrectionLevel = 'L';//容错级别
         $matrixPointSize = 6;//生成图片大小
 //生成二维码图片
         QRcode::png($value, 'qrcode.png', $errorCorrectionLevel, $matrixPointSize, 2);
-        //$logo = '/static/images/qqicon.png';//准备好的logo图片
+        $logo = 'logo.png';//准备好的logo图片
         $QR = 'qrcode.png';//已经生成的原始二维码图
 
-        /*if ($logo !== FALSE) {
-            $QR = imagecreatefromstring(file_get_contents($QR));
-            $logo = imagecreatefromstring(file_get_contents($logo));
-            $QR_width = imagesx($QR);//二维码图片宽度
-            $QR_height = imagesy($QR);//二维码图片高度
-            $logo_width = imagesx($logo);//logo图片宽度
-            $logo_height = imagesy($logo);//logo图片高度
-            $logo_qr_width = $QR_width / 5;
-            $scale = $logo_width/$logo_qr_width;
-            $logo_qr_height = $logo_height/$scale;
-            $from_width = ($QR_width - $logo_qr_width) / 2;
-            //重新组合图片并调整大小
-            imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width,
-                $logo_qr_height, $logo_width, $logo_height);
-        }*/
-//输出图片
         Header("Content-type: image/png");
         ImagePng($QR);
     }
@@ -423,6 +407,10 @@ class Finance extends MY_Controller
         );
         //发送给用户自己
         $this->finance_model->wxpost_fin($this->config->item('WX_FIN_SJTJ'),$data_msg,$this->session->userdata('login_user_id'),'www.baidu.com');
+    }
+
+    public function test2(){
+        phpinfo();
     }
 
 
