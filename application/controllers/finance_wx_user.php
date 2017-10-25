fa3593b0d457cb5fb42b51064c96054f3b501c92
ksyydream <249499109@qq.com>
2017-10-25 10:33:30 +0800

    20170913

--- application/controllers/finance_wx_user.php
+++ application/controllers/finance_wx_user.php
@@ -266,17 +266,17 @@ class Finance_wx_user extends Finwx_Controller
     }
 
     public function status_finance_save(){
-        $position_id = $this->session->userdata('login_position_id_array');
-        $permission_id = $this->session->userdata('login_permission_id');
+        $position_id = $this->session->userdata('wx_position_id_array');
+        $permission_id = $this->session->userdata('wx_permission_id');
         if($permission_id == 1 || in_array(12,$position_id)){
 
         }else{
-            redirect(site_url('/'));
+            redirect(site_url('finance_wx_user/index'));
         }
         if(!$id = $this->input->post('finance_id'))
-            redirect(site_url('/'));
+            redirect(site_url('finance_wx_user/index'));
         if(!in_array($this->input->post("status"),array(2,3,4,5,-1)))
-            redirect(site_url('/'));
+            redirect(site_url('finance_wx_user/index'));
         $res = $this->finance_wx_model->status_finance_save();
         redirect(site_url('/finance_wx_user/index'));
     }
