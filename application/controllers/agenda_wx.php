9c177d10cd8fce214f28653c6299a5e354b68d5e
ksyydream <249499109@qq.com>
2018-01-10 20:55:52 +0800

    20180110

--- application/controllers/agenda_wx.php
+++ application/controllers/agenda_wx.php
@@ -33,8 +33,8 @@ class Agenda_wx extends Agendawx_Controller
 
     public function user_login(){
 
-        die(var_dump('123'));
         $res = $this->agenda_wx_model->user_login();
+        die(var_dump($res));
         if($res==1){
             redirect('agenda_wx_user/index');
         }else{
